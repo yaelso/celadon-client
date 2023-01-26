2ac2b8e538a479bcfff5b1de898905f612aa601d
Yael <yaelechols@gmail.com>
2023-01-25 20:11:16 -0800

    Implements initial routes

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to='login'>Login</Link>
+      <Link to='signup'>Sign Up</Link>
+      <Link to='about'>About</Link>
+    </div>
+  )
+};
+
+export default Home;
