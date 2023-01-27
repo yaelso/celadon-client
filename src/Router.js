c5cc7ced58d2bd3e090cd4fa399ec60e1bf2d456
Yael <yaelechols@gmail.com>
2023-01-27 08:56:03 -0800

    Reconfigures routing

---
+++ src/Router.js
@@ -0,0 +1,34 @@
+import { Outlet, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import About from './pages/About';
+// import Profile from './pages/Profile';
+import NotFound from './pages/404';
+
+export const routes = [
+    {
+        element: <Home />,
+        path: '/',
+    },
+    {
+        element: <Login />,
+        path: '/login',
+    },
+    {
+        element: <Signup />,
+        path: '/signup',
+    },
+    {
+        element: <About />,
+        path: '/about',
+    },
+    {
+        path: '/404',
+        element: <NotFound />
+    },
+    {
+        path: '*',
+        element: <Navigate to='/404' replace />,
+    }
+]
