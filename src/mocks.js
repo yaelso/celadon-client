1734f01e9ca4495870f6c90243c5529f1bc6374d
Yael <yaelasolomon@gmail.com>
2023-02-01 16:49:26 -0800

    Modifies and extends mocks

--- src/mocks.js
+++ src/mocks.js
@@ -1,5 +1,24 @@
 export const mockUser = {
-  id: "1303530535262046",
+  id: 1303530535262046,
   name: "Yael",
-  email: "yael@sleepy.com",
+  email: "yael@celadon.com",
 };
+
+export const mockCategories = [
+  {
+    id: 1,
+    title: "Computer Science Studying",
+    description:
+      "A collection of Computer Science resources I'm currently studying",
+  },
+  {
+    id: 2,
+    title: "Work",
+    description: "A collection of work tasks I'm currently doing",
+  },
+  {
+    id: 3,
+    title: "Reading",
+    description: "A collection of books I'm currently reading",
+  },
+];
