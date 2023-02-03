fa108cd247b47901e5bad54bd755156bdc923329
Yael <yaelasolomon@gmail.com>
2023-02-02 21:15:30 -0800

    Comments out Firebase work... for now

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,36 +1,36 @@
-import app from 'firebase/app';
-import 'firebase/auth';
+// import app from 'firebase/app';
+// import 'firebase/auth';
 
-const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-}
+// const config = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+// }
 
-class Firebase {
-    constructor() {
-        app.initializeApp(config);
-        auth = app.auth();
-    }
+// class Firebase {
+//     constructor() {
+//         app.initializeApp(config);
+//         auth = app.auth();
+//     }
 
-    doCreateUserWithEmailAndPassword = (email, password) => {
+//     doCreateUserWithEmailAndPassword = (email, password) => {
 
-    }
+//     }
 
-    doSignInWithEmailAndPassword = (email, password) => {
+//     doSignInWithEmailAndPassword = (email, password) => {
 
-    }
+//     }
 
-    doSignOut = () => auth.signOut();
+//     doSignOut = () => auth.signOut();
 
-    doPasswordReset = email => auth.sendPasswordResetEmail(email);
+//     doPasswordReset = email => auth.sendPasswordResetEmail(email);
 
-    doPasswordUpdate = password => {
-        auth.currentUser.updatePassword(password);
-    }
-}
+//     doPasswordUpdate = password => {
+//         auth.currentUser.updatePassword(password);
+//     }
+// }
 
-export default Firebase;
+// export default Firebase;
