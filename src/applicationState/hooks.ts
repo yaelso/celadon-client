70990f19fdf89d64e6559c8732a5397ca6cb8958
Yael <yaelasolomon@gmail.com>
2023-02-02 20:00:13 -0800

    Adds strongly typed selector hook for appState

---
+++ src/applicationState/hooks.ts
@@ -0,0 +1,6 @@
+import { useSelector } from "react-redux";
+import { AppState } from "./appState";
+
+export const useAppState = <TModel>(
+    selectorFn: (state: AppState) => TModel,
+) => useSelector<AppState, TModel>(selectorFn);
