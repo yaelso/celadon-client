bf4b7ba7c80abfb18c9981541bba4458a4fc22e8
Yael <yaelasolomon@gmail.com>
2023-02-03 09:39:54 -0800

    Swaps style declarations for imports

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -1,34 +1,9 @@
 import React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../themes/forms';
 
 const Login: React.FC = () => {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#B6CFB6",
-      },
-      secondary: {
-        main: "#D16851",
-      },
-    },
-    typography: {
-      button: {
-        fontWeight: 600,
-      },
-    },
-  });
-
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
--- src/auth/Signup.tsx
+++ src/auth/Signup.tsx
@@ -1,33 +1,9 @@
 import React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from '../themes/forms';
 
 const Signup: React.FC = () => {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#B6CFB6",
-      },
-      secondary: {
-        main: "#D16851",
-      },
-    },
-    typography: {
-      button: {
-        fontWeight: 600,
-      },
-    },
-  });
-
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
