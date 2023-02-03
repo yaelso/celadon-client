4c6886d746e87c155bcd9dbadfad74be16618942
Yael <yaelasolomon@gmail.com>
2023-02-02 17:39:27 -0800

    Integrates AppRouter into index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { makeRoutes } from "./navigation/routes";
+import { AppRouter } from './navigation/AppRouter'
 // import './index.css';
 
 const routes = makeRoutes();
@@ -9,6 +10,8 @@ const routes = makeRoutes();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={routes.Root} />
+    <BrowserRouter basename={routes.Root}>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>
 );
