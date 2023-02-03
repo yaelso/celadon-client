c01a95a704b4dee97ba09f17bf7c11fd2fafe1a2
Yael <yaelasolomon@gmail.com>
2023-02-03 02:14:29 -0800

    Implements stand-in Home styling

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Box, Card, CardContent, Container, CssBaseline, Divider, Typography } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './themes/forms';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="login">Login</Link>
-      <Link to="signup">Sign Up</Link>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="sm">
+        <CssBaseline />
+        <Card sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }} elevation={3}>
+          <CardContent>
+            <Typography sx={ { mb: 1.5 }}>
+              Welcome to Celadon! This is filler content for the time being!
+            </Typography>
+            <Divider />
+            <Typography sx={{ mb: 1.5, marginTop: 2 }}>
+              This page will be the primary view for users that aren't logged in!
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 };
 
