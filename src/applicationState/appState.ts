819c467b3284714793588d13d4eeb09c3b53cdd6
Yael <yaelasolomon@gmail.com>
2023-02-08 21:30:55 -0800

    Adds hook useLocalStorage

--- src/applicationState/appState.ts
+++
@@ -1,8 +0,0 @@
-export type AppState = {
-    // readonly profile?: CeladonUser;
-};
-
-export const makeDefaultState = (): AppState => ({
-    // TODO - GET RID OF THIS ONCE I UN-STUB AUTH FLOW
-    // profile: mockUser,
-});
