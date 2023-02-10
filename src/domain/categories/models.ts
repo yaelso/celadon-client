102ddba33ebd9ab42fc130f82c191961897aab6e
Yael <yaelasolomon@gmail.com>
2023-02-09 23:26:14 -0800

    Makes category and checklist models relational

--- src/domain/categories/models.ts
+++ src/domain/categories/models.ts
@@ -1,6 +1,9 @@
+import { Checklist } from "../checklists/models";
+
 export type Category = {
     id: number;
     user_id: number;
     title: string;
     description: string;
+    checklists: Checklist[];
 }
