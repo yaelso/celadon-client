4c2a95b5b09ce5b3be23249f03eebb78c9c0cfe3
Yael <yaelasolomon@gmail.com>
2023-02-13 15:38:01 -0800

    Implements calendar component

---
+++ src/domain/calendar/Calendar.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import './styles.css';
+
+const UserCalendar: React.FC = () => {
+    const [date, setDate] = useState(new Date());
+
+    function onChange(nextValue) {
+        setDate(nextValue);
+    }
+
+    return (
+        <Calendar
+            onChange={onChange}
+            value={date}
+        />
+    );
+};
+
+export default UserCalendar;
