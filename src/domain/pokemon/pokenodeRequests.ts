39bf4fc02690aca576411d2e263993b704d06d07
Yael <yaelasolomon@gmail.com>
2023-02-13 20:18:53 -0800

    Clears up pokenodeRequests utility file for future development

--- src/domain/pokemon/pokenodeRequests.ts
+++ src/domain/pokemon/pokenodeRequests.ts
@@ -1,15 +1,24 @@
 const base_evo = [
-    'Bulbasaur', 'Charmander', 'Squirtle', 'Caterpie', 'Weedle', 'Pidgey', 'Rattata',
-    'Spearow', 'Ekans', 'Pikachu', 'Sandshrew', 'Nidoran♀', 'Nidoran♂', 'Clefairy', 'Vulpix', 'Jigglypuff',
-    'Zubat', 'Oddish', 'Paras', 'Venonat', 'Diglett', 'Meowth', 'Psyduck', 'Mankey', 'Growlithe', 'Poliwag',
-    'Abra', 'Machop', 'Bellsprout', 'Tentacool', 'Geodude', 'Ponyta', 'Slowpoke', 'Magnemite', "Farfetch'd",
-    'Seel', 'Grimer', 'Shellder', 'Gastly', 'Onix', 'Drowzee', 'Krabby', 'Voltorb', 'Exeggcute', 'Cubone',
-    'Hitmonlee', 'Lickitung', 'Koffing', 'Rhyhorn', 'Chansey', 'Tangela', 'Kangaskahn', 'Horsea', 'Goldeen',
-    'Staryu', 'Mr. Mime', 'Scyther', 'Jynx', 'Electabuzz', 'Magmar', ' Pinsir', 'Tauros', 'Magikarp', 'Lapras',
-    'Ditto', 'Eevee', 'Porygon', 'Omanyte', 'Kabuto', 'Aerodactyl', 'Snorlax', 'Articuno', 'Zapdos', 'Moltres', 'Dratini',
-    'Mewtwo', 'Mew'
+    'bulbasaur', 'charmander', 'squirtle', 'caterpie', 'weedle', 'pidgey', 'rattata',
+    'spearow', 'ekans', 'pikachu', 'sandshrew', 'nidoran♀', 'nidoran♂', 'clefairy', 'vulpix', 'jigglypuff',
+    'zubat', 'oddish', 'paras', 'venonat', 'diglett', 'meowth', 'psyduck', 'mankey', 'growlithe', 'poliwag',
+    'abra', 'machop', 'bellsprout', 'tentacool', 'geodude', 'ponyta', 'slowpoke', 'magnemite', "farfetch'd",
+    'seel', 'grimer', 'shellder', 'gastly', 'onix', 'drowzee', 'krabby', 'voltorb', 'exeggcute', 'cubone',
+    'hitmonlee', 'lickitung', 'koffing', 'rhyhorn', 'chansey', 'tangela', 'kangaskahn', 'horsea', 'goldeen',
+    'staryu', 'mr. mime', 'scyther', 'jynx', 'electabuzz', 'magmar', 'pinsir', 'tauros', 'magikarp', 'lapras',
+    'ditto', 'eevee', 'porygon', 'omanyte', 'kabuto', 'aerodactyl', 'snorlax', 'articuno', 'zapdos', 'moltres', 'dratini',
+    'mewtwo', 'mew'
 ];
 
 const pickRandomBase = () => {
     return Math.floor(Math.random() * ((base_evo.length) - 1) + 1);
 };
+
+const grabRandomPokemon = () => {
+    const num = pickRandomBase();
+    const selection = base_evo[num]
+    // from here, we'll check the current user's userPokemon array
+    // if `selection` exists in the array, we'll draw again!
+
+    // otherwise, this selection will be passed to the relevant calls
+}
