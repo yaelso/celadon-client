6e305a17873b687708903042d09a101a523ff7f0
Yael <yaelasolomon@gmail.com>
2023-02-13 19:17:49 -0800

    Extends user actions

--- src/domain/users/userActions.ts
+++ src/domain/users/userActions.ts
@@ -2,7 +2,10 @@ import { User } from './models';
 import { API_ROUTES } from '../../api/apiRoutes';
 import { sendApiRequest } from '../../api/module';
 
-type PostResponseData = { user: User; }
+
+type PostResponseSuccessData = { user: User; }
+type PostResponseFailureData = { details: string; }
+type PostResponseData = PostResponseSuccessData | PostResponseFailureData;
 
 export const postUser = (jwt: string) => {
   return sendApiRequest<undefined, PostResponseData>(
@@ -21,6 +24,21 @@ export const fetchUsers = (jwt: string) => sendApiRequest<undefined, User[]>(
   {
     method:  'GET',
     url:     API_ROUTES().User,
+    headers: {
+      'Authorization': `Bearer ${jwt}`,
+    },
+  },
+);
+
+
+type FetchResponseSuccessData = { user: User }
+type FetchResponseFailureData = { details: string; }
+type FetchResponseData = FetchResponseSuccessData | FetchResponseFailureData;
+
+export const fetchCurrentUser = (jwt: string) => sendApiRequest<undefined, FetchResponseData>(
+  {
+    method:  'GET',
+    url:     API_ROUTES().User_Current,
     headers: {
       'Authorization': `Bearer ${jwt}`,
     },
